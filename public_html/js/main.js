fb1bfab147f14572eaea7b3d50b22c0b026d374a
Lana Vrebac <vrebaclana@gmail.com>
2022-11-25 14:54:53 +0100

    contact-page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,45 +1,88 @@
 $(document).ready (function(){
-    //ANIMATION
+//ANIMATION
 
-    function animation() {
-        let  windowHeight = $(window).height();
+function animation() {
+let  windowHeight = $(window).height();
         let scroll = $(window).scrollTop();
         $('.animation').each(function () {
-            let position = $(this).offset().top;
-            let animationName = $(this).attr('data-animation');
-            let delay = $(this).attr('data-delay');
-            if (position < windowHeight + scroll - 100) {
-                $(this).addClass(animationName);
-                $(this).css('animation-delay', delay);
-            }
-        });
-    }
-    $(window).scroll(function () {
-        animation();
-    });
-    animation();
-    
-   if($('.team-members-slider'.length > 0))
-   {$(".team-members-slider").owlCarousel({
-           loop:true,
-           autoplay:true,
-           responsive:{
-               0:{
-                   items:1,
-                   margin:0
-               },
-               992:{
-                   items:2,
-                 margin:30  
-               }
-           }
-   });
-   }   
-   
-     
-   
-   
-   
-    
+let position = $(this).offset().top;
+        let animationName = $(this).attr('data-animation');
+        let delay = $(this).attr('data-delay');
+        if (position < windowHeight + scroll - 100) {
+$(this).addClass(animationName);
+        $(this).css('animation-delay', delay);
+}
 });
+}
+$(window).scroll(function () {
+animation();
+});
+        animation();
+        if ($('.team-members-slider'.length > 0))
+{$(".team-members-slider").owlCarousel({
+loop:true,
+        autoplay:true,
+        responsive:{
+        0:{
+        items:1,
+                margin:0
+        },
+                992:{
+                items:2,
+                        margin:30
+                }
+        }
+});
+}
+
+//form validation
+
+$('.contact-form').validate(
+{
+hightlight: function (element) {
+$(element).addClass('is-invalid').removeClass('is-valid');
+},
+        unhighlight: function (element) {
+        $(element).addClass('is-invalid').removeClass('is-valid');
+        },
+        rules: {
+        name: {
+        required: true
+
+        },
+                email: {
+                required: true,
+                        email: true
+                },
+                message: {
+                required: true
+
+
+                },
+                messages: {
+                name: {required: 'The Name* field is required'
+                },
+                        email: {required: 'The Email* field is required',
+                                email: 'Please provide a valid email'
+                        },
+                        message: {required: 'The Message* field is required'
+                        }
+                },
+                errorElement: 'p',
+                errorPlacement: function (error, element) {
+                error.appendTo(element.colosest(".form-group").find(".error-msg"));
+                }
+
+
+
+        }
+
+
+
+
+
+ });
+
+});
+
         
